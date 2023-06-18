2fc48047c536e33be9110d573298acf772cf9dc7
imlodinu <lodinu.kalugalage@student.curtin.edu.au>
2023-06-18 23:57:18 +0800

    ecs: Add `common.zig`
    This file contains common non specific definitions for the `ecs` library.

---
+++ libs/ecs/src/common.zig
@@ -0,0 +1,9 @@
+const std = @import("std");
+const builtin = @import("builtin");
+
+pub const assert = std.debug.assert;
+pub const err = std.log.err;
+
+pub const Error = error{
+    ComponentNotFound,
+};
