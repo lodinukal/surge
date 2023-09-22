2c51a9453bc0033d89e79c27d77148cc828b57ac
imlodinu <lodinukal@gmail.com>
2023-09-22 11:49:39 +0800

    add vector3 def

--- src/core/math.zig
+++ src/core/math.zig
@@ -3,3 +3,5 @@ const int_point = @import("math/int_point.zig");
 pub const IntPoint = int_point.IntPoint;
 const vector2 = @import("math/vector2.zig");
 pub const Vector2 = vector2.Vector2;
+const vector3 = @import("math/vector3.zig");
+pub const Vector3 = vector3.Vector3;
