0fcd2dfcb57fe6e1ff4db8bc3933cf92116878b3
imlodinu <lodinu.kalugalage@student.curtin.edu.au>
2023-10-09 19:11:36 +0800

    add WindowDescriptor fields, util functions, and fullscreen support

--- src/app/app.zig
+++ src/app/app.zig
@@ -16,7 +16,7 @@ pub const Application = struct {
     }
 
     pub fn initWindow(self: *Application, window: *Window, descriptor: WindowDescriptor) !void {
-        try self.platform_application.initWindow(&window.platform_window, descriptor);
+        try self.platform_application.initWindow(window, &window.platform_window, descriptor);
     }
 
     pub fn pumpEvents(self: *Application) !void {
@@ -26,11 +26,18 @@ pub const Application = struct {
 
 pub const WindowDescriptor = struct {
     title: []const u8,
-    width: ?i32 = 800,
-    height: ?i32 = 600,
+    width: i32 = 800,
+    height: i32 = 600,
+    x: ?i32 = null,
+    y: ?i32 = null,
+    is_popup: bool = false,
+    fullscreen_mode: FullscreenMode = FullscreenMode.windowed,
+    borderless: bool = false,
+    resizable: bool = true,
+    open_minimised: bool = false,
 };
 
-pub const FullscreenType = enum {
+pub const FullscreenMode = enum {
     windowed,
     borderless,
     fullscreen,
@@ -55,3 +62,43 @@ pub const Window = struct {
         self.platform_window.setShouldClose(should_close);
     }
 };
+
+pub fn enumFromValue(comptime E: type, value: anytype) E {
+    const typeInfo = @typeInfo(@TypeOf(value));
+    if (typeInfo == std.builtin.Type.EnumLiteral) {
+        return @as(E, value);
+    } else if (comptime std.meta.trait.isIntegral(@TypeOf(value))) {
+        return @enumFromInt(value);
+    } else if (@TypeOf(value) == E) {
+        return value;
+    } else {
+        @compileError("enumFromValue: invalid type: " ++ @TypeOf(value));
+    }
+}
+
+pub fn orEnum(comptime E: type, other: anytype) E {
+    var result = @intFromEnum(enumFromValue(E, other.@"0"));
+    inline for (other, 0..) |o, i| {
+        if (i == 0) continue;
+        result |= @intFromEnum(enumFromValue(E, o));
+    }
+    return @enumFromInt(result);
+}
+
+pub fn andEnum(comptime E: type, other: anytype) E {
+    var result = @intFromEnum(enumFromValue(E, other.@"0"));
+    inline for (other, 0..) |o, i| {
+        if (i == 0) continue;
+        result &= @intFromEnum(enumFromValue(E, o));
+    }
+    return @enumFromInt(result);
+}
+
+pub fn xorEnum(comptime E: type, other: anytype) E {
+    var result = @intFromEnum(enumFromValue(E, other.@"0"));
+    inline for (other, 0..) |o, i| {
+        if (i == 0) continue;
+        result ^= @intFromEnum(enumFromValue(E, o));
+    }
+    return @enumFromInt(result);
+}
--- src/app/windows.zig
+++ src/app/windows.zig
@@ -27,22 +27,22 @@ const WindowsApplication = struct {
         _ = self;
     }
 
-    pub fn initWindow(self: *WindowsApplication, window: *WindowsWindow, descriptor: app.WindowDescriptor) !void {
-        try window.init(self, descriptor);
+    pub fn initWindow(self: *WindowsApplication, base: *app.Window, window: *WindowsWindow, descriptor: app.WindowDescriptor) !void {
+        try window.init(base, self, descriptor);
     }
 
     pub fn pumpEvents(self: *WindowsApplication) !void {
         _ = self;
-        var msg: std.os.windows.user32.MSG = undefined;
-        while (try std.os.windows.user32.peekMessageW(
+        var msg: win32.ui.windows_and_messaging.MSG = undefined;
+        while (win32.ui.windows_and_messaging.PeekMessageW(
             &msg,
             null,
             0,
             0,
-            std.os.windows.user32.PM_REMOVE,
-        ) == true) {
-            _ = std.os.windows.user32.translateMessage(&msg);
-            _ = std.os.windows.user32.dispatchMessageW(&msg);
+            win32.ui.windows_and_messaging.PM_REMOVE,
+        ) != -1) {
+            _ = win32.ui.windows_and_messaging.TranslateMessage(&msg);
+            _ = win32.ui.windows_and_messaging.DispatchMessageW(&msg);
         }
     }
 
@@ -52,25 +52,30 @@ const WindowsApplication = struct {
 };
 
 const WindowsWindow = struct {
+    var class_registered: bool = false;
+    const class_name = win32.zig.L("app");
+
+    base: *app.Window = undefined,
     application: *WindowsApplication,
     hwnd: ?win32.foundation.HWND = null,
 
     should_close: bool = false,
     descriptor: app.WindowDescriptor,
 
+    non_fullscreen_window_placement: win32.ui.windows_and_messaging.WINDOWPLACEMENT = undefined,
+
     pub inline fn allocator(self: *WindowsWindow) std.mem.Allocator {
         return self.application.allocator();
     }
 
-    pub fn init(window: *WindowsWindow, application: *WindowsApplication, descriptor: app.WindowDescriptor) !void {
+    pub fn init(self: *WindowsWindow, base: *app.Window, application: *WindowsApplication, descriptor: app.WindowDescriptor) !void {
         try registerClassOnce();
-        window.application = application;
-        window.descriptor = descriptor;
-        window.hwnd = try window.buildWindow();
+        self.base = base;
+        self.application = application;
+        self.descriptor = descriptor;
+        self.hwnd = try self.buildWindow();
     }
 
-    var class_registered: bool = false;
-    const class_name = win32.zig.L("app");
     fn registerClassOnce() !void {
         if (class_registered) {
             return;
@@ -129,8 +134,8 @@ const WindowsWindow = struct {
             }),
             win32.ui.windows_and_messaging.CW_USEDEFAULT,
             win32.ui.windows_and_messaging.CW_USEDEFAULT,
-            descriptor.width orelse win32.ui.windows_and_messaging.CW_USEDEFAULT,
-            descriptor.height orelse win32.ui.windows_and_messaging.CW_USEDEFAULT,
+            descriptor.width,
+            descriptor.height,
             null,
             null,
             try getHInstance(),
@@ -165,6 +170,166 @@ const WindowsWindow = struct {
         self.should_close = should_close;
     }
 
+    const HWND_TOP: ?win32.foundation.HWND = null;
+    pub fn setFullscreen(
+        self: *WindowsWindow,
+        mode: app.FullscreenMode,
+    ) void {
+        if (mode == self.descriptor.fullscreen_mode) {
+            return;
+        }
+        const previous_mode = self.descriptor.fullscreen_mode;
+        self.descriptor.fullscreen_mode = mode;
+        var current_style = win32.ui.windows_and_messaging.GetWindowLongW(
+            self.hwnd,
+            ._STYLE,
+        );
+        var styles = self.getStyles();
+        current_style &= ~@intFromEnum(Styles.mask);
+        current_style = @bitCast(@intFromEnum(app.orEnum(
+            Styles.ws,
+            .{ current_style, styles.style },
+        )));
+        _ = win32.ui.windows_and_messaging.SetWindowLongW(
+            self.hwnd,
+            ._STYLE,
+            current_style,
+        );
+
+        if (previous_mode == .windowed and mode == .fullscreen) {
+            self.non_fullscreen_window_placement.length = @sizeOf(win32.ui.windows_and_messaging.WINDOWPLACEMENT);
+            _ = win32.ui.windows_and_messaging.GetWindowPlacement(
+                self.hwnd,
+                &self.non_fullscreen_window_placement,
+            );
+
+            var mi: win32.graphics.gdi.MONITORINFO = undefined;
+            mi.cbSize = @sizeOf(win32.graphics.gdi.MONITORINFO);
+            _ = win32.graphics.gdi.GetMonitorInfoW(
+                win32.graphics.gdi.MonitorFromWindow(
+                    self.hwnd,
+                    .NEAREST,
+                ),
+                &mi,
+            );
+
+            _ = win32.ui.windows_and_messaging.SetWindowPos(
+                self.hwnd,
+                HWND_TOP,
+                mi.rcMonitor.left,
+                mi.rcMonitor.top,
+                mi.rcMonitor.right - mi.rcMonitor.left,
+                mi.rcMonitor.bottom - mi.rcMonitor.top,
+                app.orEnum(win32.ui.windows_and_messaging.SET_WINDOW_POS_FLAGS, .{
+                    .NOOWNERZORDER,
+                    .DRAWFRAME,
+                }),
+            );
+        }
+
+        if (previous_mode == .fullscreen and mode == .windowed) {
+            _ = win32.ui.windows_and_messaging.SetWindowPlacement(
+                self.hwnd,
+                &self.non_fullscreen_window_placement,
+            );
+            _ = win32.ui.windows_and_messaging.SetWindowPos(
+                self.hwnd,
+                null,
+                0,
+                0,
+                0,
+                0,
+                app.orEnum(win32.ui.windows_and_messaging.SET_WINDOW_POS_FLAGS, .{
+                    .NOMOVE,
+                    .NOSIZE,
+                    .NOZORDER,
+                    .NOOWNERZORDER,
+                    .DRAWFRAME,
+                }),
+            );
+        }
+    }
+
+    const Styles = struct {
+        style: ws,
+        ex: wexs,
+        const ws = win32.ui.windows_and_messaging.WINDOW_STYLE;
+        const wexs = win32.ui.windows_and_messaging.WINDOW_EX_STYLE;
+        // yoinked from https://github.com/libsdl-org/SDL/blob/main/src/video/windows/SDL_windowswindow.c#L94
+        const basic = ws.initFlags(.{
+            .CLIPSIBLINGS = 1,
+            .CLIPCHILDREN = 1,
+        });
+        const fullscreen = ws.initFlags(.{
+            .POPUP = 1,
+            .GROUP = 1, // MINIMIZEBOX
+        });
+        const borderless = ws.initFlags(.{
+            .POPUP = 1,
+            .GROUP = 1, // MINIMIZEBOX
+        });
+        const borderless_windowed = ws.initFlags(.{
+            .POPUP = 1,
+            .CAPTION = 1,
+            .SYSMENU = 1,
+            .GROUP = 1, // MINIMIZEBOX
+        });
+        const normal = ws.initFlags(.{
+            .OVERLAPPED = 1,
+            .CAPTION = 1,
+            .SYSMENU = 1,
+            .GROUP = 1,
+        });
+        const resizable = ws.initFlags(.{
+            .THICKFRAME = 1,
+            .TABSTOP = 1,
+        });
+        const mask: ws = app.orEnum(ws, .{
+            fullscreen,
+            borderless,
+            normal,
+            resizable,
+        });
+    };
+
+    fn getStyles(self: *const WindowsWindow) Styles {
+        const ws = win32.ui.windows_and_messaging.WINDOW_STYLE;
+        const wexs = win32.ui.windows_and_messaging.WINDOW_EX_STYLE;
+        var styles: Styles = .{
+            .style = ws.initFlags(.{}),
+            .ex = wexs.initFlags(.{}),
+        };
+        const descriptor = self.descriptor;
+
+        if (descriptor.is_popup) {
+            styles.style = app.orEnum(ws, .{ styles.style, .POPUP });
+        } else if (descriptor.fullscreen_mode == .fullscreen) {
+            styles.style = app.orEnum(ws, .{ styles.style, Styles.fullscreen });
+        } else {
+            if (descriptor.borderless) {
+                styles.style = app.orEnum(ws, .{ styles.style, Styles.borderless_windowed });
+            } else {
+                styles.style = app.orEnum(ws, .{ styles.style, Styles.normal });
+            }
+
+            if (descriptor.resizable) {
+                if (!descriptor.borderless) {
+                    styles.style = app.orEnum(ws, .{ styles.style, Styles.resizable });
+                }
+            }
+
+            if (descriptor.open_minimised) {
+                styles.style = app.orEnum(ws, .{ styles.style, .MINIMIZE });
+            }
+        }
+
+        if (descriptor.is_popup) {
+            styles.ex = app.orEnum(wexs, .{ styles.ex, .TOOLWINDOW, .NOACTIVATE });
+        }
+
+        return styles;
+    }
+
     fn windowFromHwnd(hwnd: win32.foundation.HWND) ?*WindowsWindow {
         return @ptrFromInt(@as(
             usize,
@@ -182,24 +347,12 @@ const WindowsWindow = struct {
         lparam: std.os.windows.LPARAM,
     ) callconv(std.os.windows.WINAPI) std.os.windows.LRESULT {
         var window: *WindowsWindow = window: {
-            var window_opt: ?*WindowsWindow = null;
-            if (msg == win32.ui.windows_and_messaging.WM_NCCREATE) {
-                var cs: *win32.ui.windows_and_messaging.CREATESTRUCTW = @ptrFromInt(@as(usize, @bitCast(lparam)));
-                window_opt = @alignCast(@ptrCast(cs.lpCreateParams));
-                window_opt.?.hwnd = wnd;
-                _ = win32.ui.windows_and_messaging.SetWindowLongPtrW(
+            var window_opt: ?*WindowsWindow = @ptrFromInt(
+                @as(usize, @bitCast(win32.ui.windows_and_messaging.GetWindowLongPtrW(
                     wnd,
                     win32.ui.windows_and_messaging.GWLP_USERDATA,
-                    @bitCast(@intFromPtr(window_opt)),
-                );
-            } else {
-                window_opt = @ptrFromInt(
-                    @as(usize, @bitCast(win32.ui.windows_and_messaging.GetWindowLongPtrW(
-                        wnd,
-                        win32.ui.windows_and_messaging.GWLP_USERDATA,
-                    ))),
-                );
-            }
+                ))),
+            );
             break :window (window_opt orelse return win32.ui.windows_and_messaging.DefWindowProcW(
                 wnd,
                 msg,
@@ -212,6 +365,11 @@ const WindowsWindow = struct {
             win32.ui.windows_and_messaging.WM_CLOSE => {
                 // std.debug.print("{*}\n", .{window});
                 window.setShouldClose(true);
+            },
+            win32.ui.windows_and_messaging.WM_KEYDOWN => {
+                if (wparam == @intFromEnum(win32.ui.input.keyboard_and_mouse.VK_F)) {
+                    window.setFullscreen(if (window.descriptor.fullscreen_mode == .windowed) .fullscreen else .windowed);
+                }
             },
             else => {},
         }
