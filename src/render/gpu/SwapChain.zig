cc47812b13d1fd0d506bb2c471a031581781a929
imlodinu <lodinu.kalugalage@student.curtin.edu.au>
2023-11-22 18:42:23 +0800

    give proper function definitions for SwapChain

--- src/render/gpu/SwapChain.zig
+++ src/render/gpu/SwapChain.zig
@@ -11,12 +11,12 @@ const Self = @This();
 
 render_target: RenderTarget,
 
-fn_present: ?*const fn (*Self) void = null,
+fn_present: ?*const fn (*Self) Renderer.Error!void = null,
 fn_getCurrentSwapIndex: ?*const fn (*const Self) u32 = null,
 fn_getNumSwapBuffers: ?*const fn (*const Self) u32 = null,
 fn_getColourFormat: ?*const fn (*const Self) Renderer.format.Format = null,
 fn_getDepthStencilFormat: ?*const fn (*const Self) Renderer.format.Format = null,
-fn_resizeBuffers: ?*const fn (*Self, [2]u32) bool = null,
+fn_resizeBuffers: ?*const fn (*Self, [2]u32) Renderer.Error!void = null,
 
 surface: ?*app.window.Window = null,
 resolution: [2]u32 = .{ 0, 0 },
@@ -49,24 +49,24 @@ pub fn init(self: *Self, create_info: *const SwapChainCreateInfo) !void {
     };
 }
 
-inline fn fromBase(rt: *RenderTarget) *Self {
+pub inline fn fromBaseMut(rt: *RenderTarget) *Self {
     return @fieldParentPtr(Self, "render_target", rt);
 }
 
-inline fn fromBaseConst(rt: *const RenderTarget) *const Self {
+pub inline fn fromBase(rt: *const RenderTarget) *const Self {
     return @fieldParentPtr(Self, "render_target", @constCast(rt));
 }
 
 // RenderTarget
 fn _getResolution(rt: *const RenderTarget) [2]u32 {
-    return fromBaseConst(rt).getResolution();
+    return fromBase(rt).getResolution();
 }
 pub inline fn getResolution(self: *const Self) [2]u32 {
     return self.resolution;
 }
 
 fn _getNumColourAttachments(rt: *const RenderTarget) u32 {
-    return fromBaseConst(rt).getNumColourAttachments();
+    return fromBase(rt).getNumColourAttachments();
 }
 pub inline fn getNumColourAttachments(self: *const Self) u32 {
     _ = self;
@@ -74,14 +74,14 @@ pub inline fn getNumColourAttachments(self: *const Self) u32 {
 }
 
 fn _hasDepthAttachment(rt: *const RenderTarget) bool {
-    return fromBaseConst(rt).hasDepthAttachment();
+    return fromBase(rt).hasDepthAttachment();
 }
 pub inline fn hasDepthAttachment(self: *const Self) bool {
     return Renderer.format.isDepthFormat(self.getDepthStencilFormat());
 }
 
 fn _hasStencilAttachment(rt: *const RenderTarget) bool {
-    return fromBaseConst(rt).hasStencilAttachment();
+    return fromBase(rt).hasStencilAttachment();
 }
 pub inline fn hasStencilAttachment(self: *const Self) bool {
     return Renderer.format.isStencilFormat(self.getDepthStencilFormat());
@@ -89,7 +89,7 @@ pub inline fn hasStencilAttachment(self: *const Self) bool {
 
 // SwapChain
 pub fn present(self: *Self) void {
-    if (self.fn_present) |f| f(self);
+    if (self.fn_present) |f| f(self) catch {};
 }
 
 pub fn getCurrentSwapIndex(self: *const Self) u32 {
@@ -112,24 +112,23 @@ pub fn getDepthStencilFormat(self: *const Self) Renderer.format.Format {
     unreachable;
 }
 
-pub fn resizeBuffers(self: *Self, resolution: [2]u32, resize_info: SwapChainResizeInfo) bool {
+pub fn resizeBuffers(self: *Self, resolution: [2]u32, resize_info: SwapChainResizeInfo) Renderer.Error!void {
     if (resize_info.modify_surface) {
         if (self.surface) |s| {
             s.setFullscreenMode(if (resize_info.fullscreen) .fullscreen else .windowed);
-            s.setSize(resolution);
+            s.setSize(resolution, true);
             s.update();
-            if (self.fn_resizeBuffers.?(self, resolution)) {
+            try self.fn_resizeBuffers.?(self, resolution);
+            {
                 self.resolution = resolution;
-                return true;
             }
         }
     } else {
-        if (self.fn_resizeBuffers.?(self, resolution)) {
+        try self.fn_resizeBuffers.?(self, resolution);
+        {
             self.resolution = resolution;
-            return true;
         }
     }
-    return false;
 }
 
 pub fn setSurface(self: *Self, surface: *app.window.Window) void {
