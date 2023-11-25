3a0bcce45c312e2bce7ec3b3066ac29c4a184f86
imlodinu <lodinu.kalugalage@student.curtin.edu.au>
2023-11-25 13:55:55 +0800

    remove stale imports and fix ScratchSpace

--- src/core/common.zig
+++ src/core/common.zig
@@ -14,36 +14,16 @@ pub fn ScratchSpace(comptime len: usize) type {
         buf: [len]u8 = undefined,
         fba: std.heap.FixedBufferAllocator = undefined,
 
-        pub fn init(s: @This()) @This() {
+        pub fn init(s: *@This()) *@This() {
             s.fba = std.heap.FixedBufferAllocator.init(&s.buf);
             return s;
         }
 
-        pub fn allocator(s: @This()) std.mem.Allocator {
+        pub fn allocator(s: *@This()) std.mem.Allocator {
             return s.fba.allocator();
         }
     };
 }
-
-const point = @import("math/point.zig");
-
-pub const Vec2 = point.Vec2;
-pub const Vec3 = point.Vec3;
-
-pub const Vec2i = point.Vec2i;
-pub const Vec3i = point.Vec3i;
-pub const Vec2f = point.Vec2f;
-pub const Vec3f = point.Vec3f;
-
-const rect = @import("math/rect.zig");
-
-pub const Rect2 = rect.Rect2;
-pub const Rect3 = rect.Rect3;
-
-pub const Rect2i = rect.Rect2i;
-pub const Rect3i = rect.Rect3i;
-pub const Rect2f = rect.Rect2f;
-pub const Rect3f = rect.Rect3f;
 
 const lazy = @import("lazy.zig");
 pub const Lazy = lazy.Lazy;
